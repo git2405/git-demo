2e42cdeb2385264126141a3eed840aa3652af5df
git2405 <jayaramanmit@gmail.com>
2022-10-05 21:35:36 +0530

    My Task

---
+++ src/com/git/GitHubSample.java
@@ -0,0 +1,23 @@
+package com.git;
+
+public class GitHubSample {
+	
+	public void empName() {
+
+		System.out.println("Jai");
+	}
+
+	public void empId() {
+		
+		System.out.println("4534");
+	}
+	
+	public static void main(String[] args) {
+		
+		GitHubSample g = new GitHubSample();
+		
+		g.empId();
+		
+		g.empName();
+	}
+}
